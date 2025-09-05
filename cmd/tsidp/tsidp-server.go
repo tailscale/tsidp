5df20920cd4d413366fbce603bc1a59412c71f7c
Benson Wong <mostlygeek@gmail.com>
2025-09-05 16:05:35 -0700

    remove debugging from tsidp-server

--- cmd/tsidp/tsidp-server.go
+++ cmd/tsidp/tsidp-server.go
@@ -187,7 +187,7 @@ func main() {
 		httpServer := http.Server{
 
 			// TODO: THIS IS ONLY FOR DEBUGGING
-			Handler: wrapWithDebugLogging(srv),
+			Handler: srv,
 			ConnContext: func(ctx context.Context, c net.Conn) context.Context {
 				return context.WithValue(ctx, server.CtxConn{}, c)
 			},
@@ -209,28 +209,4 @@ func main() {
 		log.Fatalf("watcher error: %v", err)
 		return
 	}
-}
-
-// wrapWithDebugLogging returns a new http.Handler that logs request method and path
-func wrapWithDebugLogging(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Create a wrapper to capture the status code
-		wrapper := &responseWrapper{ResponseWriter: w, statusCode: 200} // default to 200
-
-		handler.ServeHTTP(wrapper, r)
-
-		// Now you can output the request method, path and the response status code
-		log.Printf("%s %s - %d", r.Method, r.URL.Path, wrapper.statusCode)
-	})
-}
-
-// responseWrapper wraps http.ResponseWriter to capture the status code
-type responseWrapper struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rw *responseWrapper) WriteHeader(statusCode int) {
-	rw.statusCode = statusCode
-	rw.ResponseWriter.WriteHeader(statusCode)
 }
